6decfe2750449ee0e443d4316ce08d30ffaf6253
mouhou15 <katihamo40@gmail.com>
2024-11-16 23:19:14 +0100

    Add files via upload
    Signed-off-by: mouhou15 <katihamo40@gmail.com>

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Merci pour votre message ! Nous vous répondrons bientôt.');
+});
